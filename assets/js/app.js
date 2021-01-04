90ad0e2e577b3a3aee1bbd037053cd1c2b62b828
Abdullah Al Mamun <abdullah914115@gmail.com>
2021-01-04 12:02:49 +0600

    Added new section on single-category page top for category details

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,12 @@ $readMoreJS.init({
 	moreLink: '<button class="btn main-color bg-white border"> <strong>Read More <i class="bi bi-chevron-down"></i></strong></button>',    // The text of 'Read more' link. Default: 'read more ...'
 	lessLink: '<button class="btn main-color bg-white border"> <strong>Show Less <i class="bi bi-chevron-up"></i></strong></button>'         // The text of 'Read less' link. Default: 'read less'
 });
+
+// listing-page read-more-btn
+$readMoreJS.init({
+	target: '#business-details-read-more',           // Selector of the element the plugin applies to (any CSS selector, eg: '#', '.'). Default: ''
+	numOfWords: 50,               // Number of words to initially display (any number). Default: 50
+	toggle: true,                 // If true, user can toggle between 'read more' and 'read less'. Default: true
+	moreLink: '<button class="btn main-color bg-white border"> <strong>Read More <i class="bi bi-chevron-down"></i></strong></button>',    // The text of 'Read more' link. Default: 'read more ...'
+	lessLink: '<button class="btn main-color bg-white border"> <strong>Show Less <i class="bi bi-chevron-up"></i></strong></button>'         // The text of 'Read less' link. Default: 'read less'
+});
